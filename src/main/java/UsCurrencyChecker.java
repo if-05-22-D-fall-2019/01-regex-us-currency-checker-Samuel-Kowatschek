df54910c94784c40b20daa1788b9c127fc10e916
Samuel-Kowatschek <samkow@gmx.at>
2020-05-25 09:17:57 +0200

    finished project

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,11 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
+    private static final String CURRENCY_PATTERN = "\\$\\s?\\d+(\\.\\d{1,2})?";
+    private static final Pattern currencyPattern = Pattern.compile(CURRENCY_PATTERN);
+
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        return currencyPattern.matcher(usCurrencyString);
     }
 }
